4f3b52e1500579846972759ca4db412c6024ee0e
Sehun2304 <131871197+Sehun2304@users.noreply.github.com>
2023-12-10 04:35:37 +0900

    [level unrated] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i =0; i*i<=n; i++){
+        if(n==i*i){
+            answer=1;
+        }else{
+            answer=2;
+        }            
+}
+        return answer;
+    }}
